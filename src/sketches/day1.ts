d7450aeec149e8803f6b246310f9c9eb446769ea
Felix Martinez <sirokosan@gmail.com>
2024-12-30 15:55:11 +0100

    added rain for day 1

--- src/sketches/day1.ts
+++ src/sketches/day1.ts
@@ -1,4 +1,5 @@
-import { Camera, Material, PlaneGeometry, Renderable, Renderer, Scene } from "kansei";
+import { Camera, Material, MouseVectors, PlaneGeometry, Renderable, Renderer, Scene } from "kansei";
+import gsap from "gsap";
 
 const canvasContainer: HTMLElement | null = document.getElementById('canvas-container');
 const camera: Camera = new Camera(45, 0.1, 100, window.innerWidth / window.innerHeight);
@@ -8,6 +9,12 @@ const renderer: Renderer = new Renderer({
     antialias: true,
     devicePixelRatio: devicePixelRatio,
 });
+const drops: Renderable[] = [];
+const dropCount = 1500;
+const ripples: Renderable[] = [];
+const rippleCount = 100;
+
+const mouseVectors: MouseVectors = new MouseVectors(canvasContainer!);
 const scene: Scene = new Scene();
 const material: Material =new Material(/* wgsl */`
     struct VertexOut {
@@ -55,15 +62,28 @@ const init = async () => {
     canvasContainer?.appendChild(renderer.canvas);
 
     camera.position.set(0, 0, 50);
-    for (let i = 0; i < 5000; i++) {
-        const cube = new Renderable(geometry, material);
-        cube.position.x = Math.floor(Math.random() * 1000 - 500);
-        cube.position.y = Math.floor(Math.random() * 60 - 30);
-        cube.position.z = Math.floor(Math.random() * 60 - 30);
-        cube.scale.x = (Math.random() + 1) * 5 ;
-        cube.scale.y = 0.1;
-        cube.rotation.x = Math.PI;
-        scene.add(cube);
+    for (let i = 0; i < dropCount; i++) {
+        const ripple = new Renderable(geometry, material);
+        ripple.position.x = Math.floor(Math.random() * 1000 - 500);
+        ripple.position.y = -15;
+        ripple.position.z = Math.floor(Math.random() * 60 - 30);
+        ripple.scale.x = (Math.random() + 1) * 5;
+        ripple.scale.y = 0.1;
+        ripple.rotation.x = Math.PI;
+        scene.add(ripple);
+        ripples.push(ripple);
+    }
+
+    for (let i = 0; i < dropCount; i++) {
+        const drop = new Renderable(geometry, material);
+        drop.position.x = Math.floor(Math.random() * 1000 - 500);
+        drop.position.y = Math.random() * 100;
+        drop.position.z = Math.floor(Math.random() * 60 - 30);
+        drop.scale.x = 0.1;
+        drop.scale.y = (Math.random() + 1) * 5;
+        drop.rotation.x = Math.PI;
+        scene.add(drop);
+        drops.push(drop);
     }
     window.addEventListener('resize', resize);
     animate();
@@ -71,10 +91,33 @@ const init = async () => {
 
 const animate = () => {
     requestAnimationFrame(animate);
+    mouseVectors.update(0.01);
+    camera.position.x = mouseVectors.mousePosition.x * 2;
+    camera.position.y = mouseVectors.mousePosition.y * -1;
     const time = performance.now() * 0.0001;
-    for (let i = 0; i < 5000; i++) {
-        const cube = scene.children[i];
-        cube.position.x = Math.sin(time + i * 0.001) * 100 - 50;
+    for (let i = 0; i < dropCount; i++) {
+        const drop = drops[i];
+        drop.position.y -= 0.7;
+        if(drop.position.y < -10) {
+            drop.position.x = Math.floor(Math.random() * 1000 - 500);
+            drop.position.y += 100;
+            ripples[i].position.x = drop.position.x;
+            ripples[i].position.z =drop.position.z;
+            ripples[i].position.y = -15;
+            ripples[i].scale.x = 0;
+            gsap.to(ripples[i].scale, {
+                x: (Math.random() + 1) * 5,
+                duration: 1.0,
+                ease: 'elastic.out',
+            });
+            gsap.to(ripples[i].scale, {
+                x: 0,
+                duration: 0.5,
+                ease: 'power1.inOut',
+                delay: 1.0,
+            });
+        }
+        // cube.position.x = Math.sin(time + i * 0.001) * 200 - 100;
     }
     renderer.render(scene, camera);
 }
