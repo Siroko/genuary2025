c29e073185ba7392bc0a52a343f80db638e779f5
Felix Martinez <sirokosan@gmail.com>
2025-01-01 17:25:49 +0100

    added layers with instancing

--- src/sketches/day2.ts
+++ src/sketches/day2.ts
@@ -1,27 +1,32 @@
-import { BufferBase, Camera, Compute, ComputeBuffer, Material, MouseVectors, PlaneGeometry, Renderable, Renderer, Scene } from "kansei";
+import { BufferBase, Camera, CameraControls, Compute, ComputeBuffer, Float, InstancedGeometry, Material, MouseVectors, PlaneGeometry, Renderable, Renderer, Scene, Vector3 } from "kansei";
 import gsap from "gsap";
 
 const canvasContainer: HTMLElement | null = document.getElementById('canvas-container');
-const camera: Camera = new Camera(45, 0.1, 100, window.innerWidth / window.innerHeight);
+const camera: Camera = new Camera(45, 0.1, 10000, window.innerWidth / window.innerHeight);
 const renderer: Renderer = new Renderer({
     width: window.innerWidth,
     height: window.innerHeight,
     antialias: true,
     devicePixelRatio: devicePixelRatio,
+    sampleCount: 4,
 });
-const drops: Renderable[] = [];
-const ripples: Renderable[] = [];
-const dropCount = 500;
 
+const layerCount = 256;
+const time = new Float(0);
 const mouseVectors: MouseVectors = new MouseVectors(canvasContainer!);
+const cameraControls: CameraControls = new CameraControls(camera, new Vector3(0, 0, 0), canvasContainer!);
 const scene: Scene = new Scene();
 const material: Material = new Material(/* wgsl */`
+    #include <curl>
     struct VertexOut {
         @builtin(position) position : vec4<f32>,
         @location(1) normal : vec3<f32>,
         @location(2) uv : vec2<f32>,
         @location(3) viewPosition : vec4<f32>,
+        @location(4) instancePosition : vec4<f32>,
     };
+
+    @group(0) @binding(0) var<uniform> time:f32;
 
     @group(1) @binding(0) var<uniform> normalMatrix:mat4x4<f32>;
     @group(1) @binding(1) var<uniform> worldMatrix:mat4x4<f32>;
@@ -33,11 +38,18 @@ const material: Material = new Material(/* wgsl */`
     fn vertex_main(
         @location(0) position: vec4<f32>,
         @location(1) normal : vec3<f32>,
-        @location(2) uv : vec2<f32>
+        @location(2) uv : vec2<f32>,
+        @builtin(instance_index) instanceID : u32,
     ) -> VertexOut
     {
         var output : VertexOut;
-        output.position = projectionMatrix * viewMatrix * worldMatrix * position;
+        var p = position;
+        p.z -= (f32(instanceID) * 0.7) - 100.0;
+        // p.y += sin(time + p.z * 0.07) * 10.7;
+        // p.x += cos(time + p.z * 0.07) * 10.7;
+        output.instancePosition = p;
+        output.position = projectionMatrix * viewMatrix * worldMatrix * p;
+        
         output.normal = (worldMatrix * vec4<f32>(normal, 0.0)).xyz;
         output.uv = uv;
         output.viewPosition = viewMatrix * worldMatrix * position;
@@ -47,43 +59,42 @@ const material: Material = new Material(/* wgsl */`
     @fragment
     fn fragment_main(fragData: VertexOut) -> @location(0) vec4<f32>
     {
-        return vec4<f32>(1.0, 1.0, 1.0, 1.0);
+        let minEdge = 0.006;
+        let maxEdge = 0.008;
+        // var n = getCurlVelocity(vec4<f32>(fragData.uv, fragData.instancePosition.z, 0.0));
+        var edgeLX = smoothstep(minEdge, maxEdge, fragData.uv.x);
+        var edgeRX = smoothstep(minEdge, maxEdge, 1.0 - fragData.uv.x);
+        var edgeX = min(edgeLX, edgeRX);
+        var edgeTY = smoothstep(minEdge, maxEdge, fragData.uv.y);
+        var edgeBY = smoothstep(minEdge, maxEdge, 1.0 - fragData.uv.y);
+        var edgeY = min(edgeTY, edgeBY);
+        var edge = min(edgeX, edgeY);
+ 
+        return vec4<f32>(1.0, 1.0, 1.0, 1.1 - edge);
     }`,
     {
-        bindings: [],
+        bindings: [
+            {
+                binding: 0,
+                visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
+                value: time,
+            },
+        ],
+        cullMode: 'none',
+        transparent: true,
     }
 );
 
-const geometry: PlaneGeometry = new PlaneGeometry(1, 1);
+const geometry: PlaneGeometry = new PlaneGeometry(10, 10, 1, 1);
+const layersInstanced: InstancedGeometry = new InstancedGeometry(geometry, layerCount);
 
 const init = async () => {
     await renderer.initialize();
     canvasContainer?.appendChild(renderer.canvas);
-
-    camera.position.set(0, 0, 50);
-    for (let i = 0; i < dropCount; i++) {
-        const ripple = new Renderable(geometry, material);
-        ripple.position.x = 0;
-        ripple.position.y = 0;
-        ripple.position.z = 0;
-        ripple.scale.x = 0;
-        ripple.scale.y = 0.1;
-        ripple.rotation.x = Math.PI;
-        scene.add(ripple);
-        ripples.push(ripple);
-    }
-
-    for (let i = 0; i < dropCount; i++) {
-        const drop = new Renderable(geometry, material);
-        drop.position.x = Math.floor(Math.random() * 500 - 250);
-        drop.position.y = Math.random() * 100;
-        drop.position.z = Math.floor(Math.random() * 60 - 30);
-        drop.scale.x = 0.1;
-        drop.scale.y = (Math.random() + 1) * 5;
-        drop.rotation.x = Math.PI;
-        scene.add(drop);
-        drops.push(drop);
-    }
+    
+    const layerRenderable: Renderable = new Renderable(layersInstanced, material);
+    scene.add(layerRenderable);
+    
     window.addEventListener('resize', resize);
     animate();
 }
@@ -96,41 +107,11 @@ const animate = () => {
     const currentTime = performance.now();
     const deltaTime = (currentTime - lastTime) * 0.001; // Convert to seconds
     lastTime = currentTime;
+    time.value += deltaTime;
 
     mouseVectors.update(deltaTime);
-    camera.position.x = mouseVectors.mousePosition.x * 2;
-    camera.position.y = mouseVectors.mousePosition.y * -1;
+    cameraControls.update(deltaTime);
 
-    for (let i = 0; i < dropCount; i++) {
-        const drop = drops[i];
-        drop.position.y -= 1.2 * deltaTime * 60; // Scale movement by deltaTime and normalize to 60fps
-        if(drop.position.y < -10) {
-            const ripple = ripples[i];
-            
-            ripple.position.x = drop.position.x;
-            ripple.position.z = drop.position.z;
-            ripple.position.y = drop.position.y - (drop.scale.y * 0.5);
-            ripple.scale.x = 0;
-            ripple.scale.y = 0.1;
-
-            drop.position.x = Math.floor(Math.random() * 150 - 75);
-            drop.position.z = Math.floor(Math.random() * 60 - 30);
-            drop.position.y += 90;
-
-            const timeRandom = Math.random() * 0.25 + 0.75;
-            gsap.to(ripple.scale, {
-                x: (Math.random() + 1) * 5,
-                duration: timeRandom,
-                ease: 'elastic.out',
-            });
-            gsap.to(ripple.scale, {
-                y: 0,
-                duration: 0.2,
-                ease: 'power1.inOut',
-                delay: 0.1,
-            });
-        }
-    }
     renderer.render(scene, camera);
 }
 
