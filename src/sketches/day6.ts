e0af113b092417520d5b3155aa06145371565224
Felix Martinez <sirokosan@gmail.com>
2025-01-07 02:23:30 +0100

    added automovement

--- src/sketches/day6.ts
+++ src/sketches/day6.ts
@@ -16,9 +16,9 @@ const canvasContainer: HTMLElement | null = document.getElementById('canvas-cont
 
 // Initialize camera with perspective projection
 // Parameters: FOV (degrees), near plane, far plane, aspect ratio
-const camera: Camera = new Camera(0.01, 500, 30000, window.innerWidth / window.innerHeight);
-
-const cameraControls = new CameraControls(camera, new Vector3(0, 90, 0), canvasContainer!, 10000.5);
+const camera: Camera = new Camera(70, 1, 3000, window.innerWidth / window.innerHeight);
+const cameraTarget = new Vector3(0, 90, 0);
+const cameraControls = new CameraControls(camera, cameraTarget, canvasContainer!, 250.5);
 
 // Create WebGPU renderer with configuration
 const renderer: Renderer = new Renderer({
@@ -48,13 +48,14 @@ struct VertexOut {
     @location(2) uv : vec2<f32>,
     @location(3) viewPosition : vec4<f32>,
     @location(4) worldPosition : vec4<f32>,
-    @location(5) fakeAO : f32
+    @location(5) fakeAO : f32,
+    @location(6) distanceToTarget : f32
 };
 
 @group(0) @binding(0) var<uniform> mousePosition: vec2<f32>;
 @group(0) @binding(1) var<uniform> mouseStrength: f32;
 @group(0) @binding(2) var<uniform> time: f32;
-
+@group(0) @binding(3) var<uniform> cameraTarget: vec3<f32>;
 @group(1) @binding(0) var<uniform> normalMatrix:mat4x4<f32>;
 @group(1) @binding(1) var<uniform> worldMatrix:mat4x4<f32>;
 
@@ -76,8 +77,13 @@ fn vertex_main(
     let instance = f32(instanceID);
     let posGridX = (instance % rows) - rows * 0.5;
     let posGridY = (instance / rows) - rows * 0.5;
-    let noiseZoom = vec2<f32>(0.01);
-    let noise = fbm((vec2<f32>(posGridX, posGridY) + vec2<f32>(1024.0 + 2.0)) * noiseZoom);
+    let offsetsGrid = floor(cameraTarget.z / 128.0);
+    var positionBox = vec4<f32>(posGridX, 0.0, posGridY + offsetsGrid * 128.0, 0.0);
+
+    let noiseZoom = vec2<f32>(0.005);
+    var noise = fbm((vec2<f32>(positionBox.x, positionBox.z) + vec2<f32>(1024.0 + 2.0)) * noiseZoom);
+    // let noise2 = fbm((vec2<f32>(positionBox.x, positionBox.z) + vec2<f32>(1024.0 + 2.0)) * noiseZoom * 0.1);
+    // noise *= noise2;
     if(position.y > 0.0) {
       output.fakeAO = 1.0;
     } else {
@@ -85,9 +91,15 @@ fn vertex_main(
     }
     var pos = position;
     pos.y *= 10.0;
-    pos.y += noise * 100.0;
+    pos.y += noise * 200.0;
 
-    var offsetVertex: vec4<f32> = pos + vec4<f32>(posGridX, 0.0, posGridY, 0.0);
+    
+   
+    
+    var offsetVertex: vec4<f32> = pos + positionBox;
+
+    let distanceToTarget = distance(offsetVertex.xyz, cameraTarget.xyz);
+    output.distanceToTarget = distanceToTarget;
 
     output.viewPosition = viewMatrix * worldMatrix * offsetVertex;
     output.worldPosition = worldMatrix * offsetVertex;
@@ -106,8 +118,9 @@ fn fragment_main(fragData: VertexOut) -> @location(0) vec4<f32>
     let light = normalize(vec3<f32>(100.0, 100.0, 10.0));
     var lightDir = (dot(fragData.normal, light) + 1.3) * 0.5;
     lightDir *= fakeAO;
+    let fog = 1.0 - clamp(fragData.distanceToTarget / 512.0, 0.0, 1.0);
+    lightDir *= fog;
     var color: vec4<f32> = vec4<f32>(1.0 * lightDir, 1.0 * lightDir, 1.0 * lightDir, 1.0);
-    // color.rgb *= vec3<f32>(lightDir);
    
     return color;
 } 
@@ -138,6 +151,11 @@ const init = async () => {
           binding: 2,
           visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
           value: time
+        },
+        {
+          binding: 3,
+          visibility: GPUShaderStage.VERTEX,
+          value: cameraTarget
         }
       ],
       cullMode: 'none'
@@ -162,6 +180,7 @@ const animate = () => {
     lastTime = currentTime;
     time.value += deltaTime;
 
+    cameraTarget.z -= 1.0;
     // Update interaction systems
     cameraControls.update(deltaTime);
     mouseVectors.update(deltaTime);
